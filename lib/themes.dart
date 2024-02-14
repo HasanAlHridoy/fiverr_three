0068f69cde401d5b4695b076dbf7379845203a59
hasan <hasan15-13692@diu.edu.bd>
2024-02-14 20:49:47 +0600

    color and image setup done

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  Color get blue => const Color(0xFF1766FF);
+  Color get bgColor => const Color(0xFF2D313F);
+  Color get cardColor => const Color(0xFF373C4D);
+  Color get textTitleColor => const Color(0xFFECF0FF);
+  Color get textSubtitleColor => const Color(0xFFAFB6D3);
+  Color get richTextColor => const Color(0xFFF7C2A2);
+  Color get inactiveTextButtonColor => const Color(0xFF727997);
 }
